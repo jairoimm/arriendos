801073ff572ddbd56905090f244323177739f78b
jairo munoz <jairo.imm@gmail.com>
2024-09-16 20:40:08 -0300

    se crea modelos userprofile, region, comuna, inmueble

--- arriendos/urls.py
+++ arriendos/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    Path('', include('main.urls'))
-]
+    path('', include('main.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    ]
