5eb8fd6e1dec50e54ed4be8c1b924a8f28d8b265
jairo munoz <jairo.imm@gmail.com>
2024-09-18 17:52:24 -0300

    se crea funcion para registrar usuarios

--- main/services.py
+++ main/services.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from django.db.utils import IntegrityError
 from django.db.models import Q
 from django.db import connection
+from django.contrib import messages
 
 
 def crear_inmueble(nombre:str, descripcion:str, m2_construidos:int, m2_totales:int, num_estacionamientos:int, num_habitaciones:int, num_baños:int, direccion:str, precio_mensual_arriendo:int, tipo_de_inmueble:str, comuna_cod:str, rut_propietario:str):
@@ -50,8 +51,9 @@ def editar_inmueble(inmueble_id:int, nombre:str, descripcion:str, m2_construidos
 
 
 
-def crear_user(username:str, first_name:str, last_name:str, email:str, password:str, pass_confirm:str, direccion:str, rol:str='arrendatario', telefono:str=None) -> bool:
+def crear_user(request, username:str, first_name:str, last_name:str, email:str, password:str, pass_confirm:str, direccion:str, rol:str, telefono:str=None) -> bool:
     if password != pass_confirm:
+        messages.error(request, 'Las contraseñas no coinciden')
         return False
     try:
         user = User.objects.create_user(
@@ -62,6 +64,10 @@ def crear_user(username:str, first_name:str, last_name:str, email:str, password:
             last_name=last_name,
         )
     except IntegrityError:
+        messages.error(request, 'El rut ya está ingresado')
+        return False
+    except ValidationError:
+        messages.error(request, 'El email ya existe')
         return False
     UserProfile.objects.create(
         direccion=direccion,
@@ -69,6 +75,7 @@ def crear_user(username:str, first_name:str, last_name:str, email:str, password:
         rol = rol,
         user=user
     )
+    messages.success(request, 'Usuario creado con éxito! Por favor, ingrese')
     return True
 
 
--- main/views.py
+++ main/views.py
@@ -1,5 +1,40 @@
 from django.shortcuts import render
+from main.services import crear_user
+from django.contrib import messages
+
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html',)
+
+
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        email = request.POST['email']
+        direccion = request.POST['direccion']
+        telefono = request.POST['telefono']
+        rol = request.POST['rol']
+        password = request.POST['password']
+        password_repeat = request.POST['password_repeat']
+            
+        crear = crear_user(request, username, first_name, last_name, email, password, password_repeat, direccion, rol, telefono)
+
+        if crear: # Si crear es True
+            messages.success(request, 'Usuario creado con éxito. Por favor ingrese.')
+            return redirect('/accounts/login') 
+        # Si llegó, crear fue False
+        messages.warning(request, 'Por favor, revise los datos ingresados')
+        return render(request, 'registration/register.html', {
+            'username': username,
+            'first_name': first_name,
+            'last_name': last_name,
+            'email': email,
+            'direccion': direccion,
+            'telefono': telefono,
+            'rol': rol,
+            })
+    else: # en caso de metodo GET
+        return render(request, 'registration/register.html')
